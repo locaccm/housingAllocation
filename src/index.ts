7036d2c6dbe58a498f21133e39c02408d23d54a9
ClemZi02 <clement.foulon19@gmail.com>
2025-04-30 19:35:06 +0200

    [AIC-16] - update index to add route

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import leaseRoutes from "./routes/leaseRoute";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/lease", leaseRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
